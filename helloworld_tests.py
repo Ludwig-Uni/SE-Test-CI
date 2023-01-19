514c95aff2f6299d4ec9a7a2f350eb9a210cd69c
Ludwig-Uni <ludwig.kolesch@student.uni-tuebingen.de>
2023-01-19 13:19:30 +0100

    Fix formatting of the tests

--- helloworld_tests.py
+++ helloworld_tests.py
@@ -2,7 +2,7 @@
 import unittest
 import helloworld
 
-class TestHelloWorld(unittest.TestCase):
 
+class TestHelloWorld(unittest.TestCase):
     def test_hello_world(self):
         self.assertEqual(helloworld.hello_world(), "Hello, world!")
